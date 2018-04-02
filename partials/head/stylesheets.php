8a839a9518d0af394bbcb020ee10d2df909888af
Jessica Felts <thejessicafelts@gmail.com>
2018-04-01 21:23:29 -0400

    updated income tax calculator stylesheet files."

--- partials/head/stylesheets.php
+++ partials/head/stylesheets.php
@@ -2,12 +2,12 @@
 <?php $uri = $_SERVER['REQUEST_URI']; ?>
 
 <!-- Bootstrap -->
-<link rel="stylesheet" href="<?php echo "http://" . $server . $uri . "vendors/bootstrap/css/bootstrap-reboot.min.css"; ?>">
-<link rel="stylesheet" href="<?php echo "http://" . $server . $uri . "vendors/bootstrap/css/bootstrap-grid.min.css"; ?>">
-<link rel="stylesheet" href="<?php echo "http://" . $server . $uri . "vendors/bootstrap/css/bootstrap.min.css"; ?>">
+<link rel="stylesheet" href="<?php echo "http://localhost:8888/applications/income-tax-calculator/vendors/bootstrap/css/bootstrap-reboot.min.css"; ?>">
+<link rel="stylesheet" href="<?php echo "http://localhost:8888/applications/income-tax-calculator/vendors/bootstrap/css/bootstrap-grid.min.css"; ?>">
+<link rel="stylesheet" href="<?php echo "http://localhost:8888/applications/income-tax-calculator/vendors/bootstrap/css/bootstrap.min.css"; ?>">
 
 <!-- Font Awesome -->
-<link rel="stylesheet" href="<?php echo "http://" . $server . $uri . "vendors/fontawesome/css/font-awesome.min.css"; ?>">
+<link rel="stylesheet" href="<?php echo "http://localhost:8888/applications/income-tax-calculator/vendors/fontawesome/css/font-awesome.min.css"; ?>">
 
 <!-- Custom Styles -->
-<link rel="stylesheet" href="<?php echo "http://" . $server . $uri . "assets/css/main.css"; ?>">
+<link rel="stylesheet" href="<?php echo "http://localhost:8888/applications/income-tax-calculator/assets/css/main.css"; ?>">
